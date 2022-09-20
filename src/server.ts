cead5690dc359ac06fb9b1b0d1abc37cb75f2e19
asror797 <asror797shahobiddinov@gmail.com>
2022-09-20 22:50:07 +0500

    swagger added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express  from 'express'
 const PORT:number = 9000
+import swaggerUi from 'swagger-ui-express'
 const app: express.Application= express()
 import './config/ormconfig'
 import router from './routes/routes'
+import swaggerDocument from './swagger.json'
 
 
 app.use(router)
+
+app.use('/swagger',swaggerUi.serve,swaggerUi.setup(swaggerDocument))
+
 
 
 app.listen(PORT,() => {
